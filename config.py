2db91d467dc4302106eca0401015567a7665c5d0
Chekuri Viroopaksh <69083163+KABALI-25@users.noreply.github.com>
2020-09-20 21:40:45 +0530

    Add files via upload
    git checkout 12e

--- config.py
+++ config.py
@@ -4,15 +4,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    #MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-        ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+    #    ['true', 'on', '1']
+    MAIL_PORT=465
+    MAIL_USE_SSL=True
+    
+    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME='chekuriviroopaksh@gmail.com'
+    MAIL_PASSWORD='ztgsqofegocdhjxd'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'chekuriviroopaksh@gmail.com'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = 'Tester'
+    FLASKY_ADMIN =os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
